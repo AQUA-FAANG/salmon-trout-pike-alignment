1131b141fcbabf8f00d17d949425c841af4f6011
Lars Grønvold <lars.gronvold@gmail.com>
2024-04-23 15:24:27 +0200

    added argument parser for purge script

--- compare_alignments/purgeSameChromAlignments.py
+++ compare_alignments/purgeSameChromAlignments.py
@@ -1,30 +1,45 @@
 
+import argparse
 
-#def purgeSameChromAlifnments(inFile, outFile):
+def purgeSameChromAlignments(inFile, outFile):
 
-inFile = "compare_alignments/Ssal25.maf"
-outFile = "compare_alignments/Ssal25_filtered.maf"
+  file_in = open(inFile,"r")
+  file_out = open(outFile,"w")
 
-file_in = open(inFile,"r")
-file_out = open(outFile,"w")
+  lineNr = 0
+  skippedLines = 0
 
-lineNr = 0
-skippedLines = 0
+  line = file_in.readline()
+  for line in file_in.readlines():
+    lineNr = lineNr + 1
+    if line.startswith("a"):
+      seen_ids = []
+    if line.startswith("s"):
+      id = line.split("\t")[1]
+      if id in seen_ids:
+        skippedLines = skippedLines + 1
+        continue
+      seen_ids.append(id)
+    bytes_written = file_out.write(line)
 
-line = file_in.readline()
-for line in file_in.readlines():
-  lineNr = lineNr + 1
-  if line.startswith("a"):
-    seen_ids = []
-  if line.startswith("s"):
-    id = line.split("\t")[1]
-    if id in seen_ids:
-      skippedLines = skippedLines + 1
-      continue
-    seen_ids.append(id)
-  bytes_written = file_out.write(line)
+  file_in.close()
+  file_out.close()
 
-file_in.close()
-file_out.close()
+  print(f'Lines read:{lineNr}, lines skipped: {skippedLines}')
 
-print(f'Lines read:{lineNr}, lines skipped: {skippedLines}')
+def main():
+    # Set up the argument parser
+    parser = argparse.ArgumentParser(description='Remove multiple alignments to same chromosome.')
+    
+    # Add arguments
+    parser.add_argument('inFile', type=str, help='Input .maf file path')
+    parser.add_argument('outFile', type=str, help='Output .maf file path')
+    
+    # Parse the arguments
+    args = parser.parse_args()
+    
+    # Call the function with command line arguments
+    purgeSameChromAlignments(args.inFile, args.outFile)
+
+if __name__ == '__main__':
+    main()
